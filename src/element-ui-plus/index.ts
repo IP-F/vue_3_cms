127152e6dae436c76c26558d68a85b9fb31f15db
qianjy <1163123808@qq.com>
2021-12-09 12:59:25 +0800

    feat(global): add element-ui-plus

--- src/element-ui-plus/index.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from '@/App.vue'
-import { ElButton } from 'element-plus'
-const app = createApp(App)
-
-app.use(ElButton)
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
-const path = require('path')
-console.log('path: ', path);
+const AutoImport = require('unplugin-auto-import/webpack')
+const Components = require('unplugin-vue-components/webpack')
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = {
-    outputDir: './builds'
+    outputDir: './builds',
+
+    configureWebpack: config => {
+        // 引入注销控制台输出的插件
+        config.plugins.push(
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        )
+    }
 }
